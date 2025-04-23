3a5f3735e05b21ea5e15182aaed5eb9ea9096a63
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-04-23 22:30:53 +0100

    app js small update

--- server/app.js
+++ server/app.js
@@ -8,25 +8,19 @@ const app = express();
 const PORT = 3000;
 
 const authRoutes = require('./routes/authRoutes');
-const protectedRoutes = require('./routes/protectedRoutes');
 const bookRoutes = require('./routes/bookRoutes');
 const categoryRoutes = require('./routes/categoryRoutes');
 
 
 app.use(cors());
 app.use(express.json());
-// app.use(express.urlencoded({ extended : true }));
 
 app.use('/auth', authRoutes);
-app.use('/api', protectedRoutes);
 app.use('/books', bookRoutes);
 app.use('/categories', categoryRoutes);
 
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.get('/', (req,res)=>{
-    res.send('connected in app');
-});
 
 app.listen(PORT, () => {
     console.log('server running');
