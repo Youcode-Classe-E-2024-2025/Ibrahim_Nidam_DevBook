7e7f760f50734dd9678c9ed53f96e869f2bad665
Ibrahim-Nidam <ibrahim.nidam22@gmail.com>
2025-04-23 22:27:40 +0100

    category routes added to app js

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const connection = require('../config/db');
+const path = require('path');
 
 const app = express();
 const PORT = 3000;
@@ -9,13 +10,19 @@ const PORT = 3000;
 const authRoutes = require('./routes/authRoutes');
 const protectedRoutes = require('./routes/protectedRoutes');
 const bookRoutes = require('./routes/bookRoutes');
+const categoryRoutes = require('./routes/categoryRoutes');
+
 
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
+// app.use(express.urlencoded({ extended : true }));
 
 app.use('/auth', authRoutes);
 app.use('/api', protectedRoutes);
 app.use('/books', bookRoutes);
+app.use('/categories', categoryRoutes);
+
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/', (req,res)=>{
     res.send('connected in app');
